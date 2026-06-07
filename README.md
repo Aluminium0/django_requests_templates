f8cc39e1c7bb2841a1be5961f2058ad3c40845be
Test <test@boss.com>
2026-06-07 14:15:24 +0300

    Add recipe calculator homework

---
+++ README.md
@@ -0,0 +1,17 @@
+# Django Request Template
+
+Recipe calculator homework for Django.
+
+## Run
+
+```bash
+pip install -r requirements.txt
+python3 manage.py runserver
+```
+
+## Pages
+
+- `/omlet/` - omlet recipe for one serving
+- `/omlet/?servings=4` - omlet recipe for four servings
+- `/pasta/` - pasta recipe
+- `/buter/` - sandwich recipe
---
+++ calculator/views.py
@@ -0,0 +1,41 @@
+from django.http import Http404
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+
+def recipe_view(request, dish):
+    if dish not in DATA:
+        raise Http404('Recipe not found')
+
+    try:
+        servings = int(request.GET.get('servings', 1))
+    except ValueError:
+        servings = 1
+    if servings < 1:
+        servings = 1
+
+    recipe = {
+        ingredient: amount * servings
+        for ingredient, amount in DATA[dish].items()
+    }
+    context = {
+        'recipe': recipe,
+    }
+    return render(request, 'calculator/index.html', context)
